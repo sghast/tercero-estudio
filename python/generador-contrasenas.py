8cf0aebba32bc4c73ed20854c6ee36d9a5a6f73c
Alan <alanjavierreyes318@gmail.com>
2026-03-04 09:36:10 -0500

    Generador de contraseñas TEMPORLES seguras

---
+++ python/generador-contrasenas.py
@@ -0,0 +1,25 @@
+import random
+import string
+
+puntuacion_validos = "!#$%&@" 
+universo = string.ascii_lowercase + string.ascii_uppercase + string.digits + puntuacion_validos
+
+contrasena = ""
+
+#caracteres obligatorios: mayúscula, minúscula, número y puntuación
+obligatorios = []
+mayuscula = obligatorios.append(random.choice(string.ascii_uppercase))
+minuscula = obligatorios.append(random.choice(string.ascii_lowercase))
+numero = obligatorios.append(random.choice(string.digits))
+puntuacion = obligatorios.append(random.choice(puntuacion_validos))
+
+for i in range(8): #la longitud de la contraseña puede variar
+    caracter = obligatorios.append(random.choice(universo))
+
+random.shuffle(obligatorios) 
+#con los caracteres obligatorios incluidos se mezcla para no seguir el mismo patrón (mayuscula > minuscula > numero > puntucuacion)
+
+for i in range (len(obligatorios)):
+    contrasena += obligatorios[i]
+
+print(f"Tu contraseña es: {contrasena}\nNO LA COMPARTAS CON NADIE")
