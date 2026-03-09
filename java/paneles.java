4fed0a7884284ef7223d8ff5c0511c346e7677f1
Alan <alanjavierreyes318@gmail.com>
2026-03-09 09:25:21 -0500

    Cálculo de IMC

--- java/paneles.java
+++ java/paneles.java
@@ -1,13 +1,24 @@
 import javax.swing.JOptionPane;
 
 public class paneles {
-    public static void main (String args []) {
+    public static void main(String[] args) {
         String nombre = JOptionPane.showInputDialog("Ingresa tu nombre");
         int edad = Integer.parseInt(JOptionPane.showInputDialog("Ingresa tu edad"));
+        String ciudad = JOptionPane.showInputDialog("Ingresa tu ciudad");
+        String carrera = JOptionPane.showInputDialog("Ingresa tu carrera");
+        double estatura = Double.parseDouble(JOptionPane.showInputDialog("Ingresa tu estatura en metros"));
+        double peso = Double.parseDouble(JOptionPane.showInputDialog("Ingresa tu peso en kg"));
 
-        JOptionPane.showMessageDialog(null, "Tu nombre es " + nombre + ", tienes " + edad + " años", 
-        "datos", JOptionPane.DEFAULT_OPTION);
+        double imc = peso / (estatura * estatura);
+
+        String mensaje = "Nombre: " + nombre +
+                         "\nEdad: " + edad +
+                         "\nCiudad: " + ciudad +
+                         "\nCarrera: " + carrera +
+                         "\nEstatura: " + estatura + " m" +
+                         "\nPeso: " + peso + " kg" +
+                         "\nIMC: " + imc;
+
+        JOptionPane.showMessageDialog(null, mensaje, "Datos registrados", JOptionPane.INFORMATION_MESSAGE);
     }
 }
-
-// JOptionPane pasa avalores string, por lo que se hace la conversión con Integer.parseInt()
